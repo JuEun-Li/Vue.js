a4dd3d97c869a9b447b639c002bd0727c703d357
JuEun-Li <wndms61@gmail.com>
2020-05-04 09:41:11 +0900

    중간시험

--- dlwndms61/public/exam_01/mid1.js
+++ dlwndms61/public/exam_01/mid1.js
@@ -2,11 +2,11 @@ let a = [];
 let sum = 0;
 let avg = 0;
 
-for(let i = 0; i < 100; i++) {
+for (let i = 0; i < 100; i++) {
     a[i] = Math.floor(Math.random() * 100 + 1);
     sum = sum + a[i];
 }
-for(let i = 0; i < 100; i++) {
+for (let i = 0; i < 100; i++) {
     sum = sum + a[i];
 }
 
