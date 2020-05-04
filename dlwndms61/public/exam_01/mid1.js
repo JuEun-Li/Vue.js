834fbca950564378379adc3042853988e0ba423e
JuEun-Li <wndms61@gmail.com>
2020-05-04 09:21:27 +0900

    중간시험

---
+++ dlwndms61/public/exam_01/mid1.js
@@ -0,0 +1,14 @@
+let a = [];
+let sum = 0;
+let avg = 0;
+
+for(let i = 0; i < 100; i++) {
+    a[i] = Math.floor(Math.random() * 100 + 1);
+    sum = sum + a[i];
+}
+for(let i = 0; i < 100; i++) {
+    sum = sum + a[i];
+}
+
+avg = (sum / 100).toFixed(1);
+console.log(avg);
