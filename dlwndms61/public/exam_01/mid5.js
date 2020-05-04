009e565e89868eef0bba0ed1d52b3e479d5d7b1a
JuEun-Li <wndms61@gmail.com>
2020-05-04 10:20:42 +0900

    중간시험

--- dlwndms61/public/exam_01/mid5.js
+++ dlwndms61/public/exam_01/mid5.js
@@ -1,6 +1,17 @@
 let a = [];
+let is_even = 0;
 let sum_even = 0;
+
 for(let i = 0; i < 100; i++) {
     a[i] = Math.floor(Math.random() * 100 + 1);
 }
-a.filter
+
+function callback(a, b) {  
+  return a + b; 
+}
+
+is_even = a.filter((e) => e % 2 == 0);
+sum_even = is_even.reduce(callback);
+
+console.log(a);
+console.log(sum_even);
