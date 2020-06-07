69f4afdc11f6eac1daeb552c5c245272b5d50d8a
JuEun-Li <wndms61@gmail.com>
2020-06-08 04:06:17 +0900

    app.vue

---
+++ dlwndms61/vue_app2/src/pages/page1/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <h1>페이지1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
---
+++ dlwndms61/vue_app2/src/pages/page1/Counter1.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>
+      {{ title }} -
+      <small>{{ description }}</small>
+    </h2>
+    <input type="number" v-model="counter" />
+    <button type="button" @click="counter++">++</button> <!--@click은 v-on:click과 같다-->
+    <button type="button" @click="counter--">--</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter1", // 컴포넌트 이름(파일명이 달라도 괜찮다.)
+  props: ["title", "description"], // 파라미터와 비슷하다.
+  data: function() {
+    return { counter: 0 };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid gray;
+  padding: 0 20px 20px 20px;
+  margin-top: 10px;
+}
+input {
+  padding: 5px;
+  margin-right: 5px;
+}
+button {
+  padding: 0.4em 1em;
+  margin-right: 5px;
+}
+</style>
+
