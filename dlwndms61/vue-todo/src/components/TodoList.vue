99716e62ab8773118a1da146fdca6acbdd8fed5c
JuEun-Li <wndms61@gmail.com>
2020-11-04 22:35:10 +0900

    list

--- dlwndms61/vue-todo/src/components/TodoList.vue
+++ dlwndms61/vue-todo/src/components/TodoList.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-      list 
+      <ul>
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+          {{ todoItem }}
+        </li>
+        <!-- <li>2</li> -->
+        <!-- <li>3</li> -->
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      } // localstorage 데이터 꺼내기
+    }
+  }
 }
 </script>
 
