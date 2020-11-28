84494c61b7e3fca1b9c747c0009de6038abd4694
JuEun-Li <wndms61@gmail.com>
2020-11-29 00:04:28 +0900

    .

--- myProject_1/page_01/src/App.vue
+++ myProject_1/page_01/src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageHeader></PageHeader>
+    <PageMain></PageMain>
+    <PageFooter></PageFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/PageHeader.vue'
+import PageMain from './components/PageMain.vue'
+import PageFooter from './components/PageFooter.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    'PageHeader': PageHeader,
+    'PageMain': PageMain,
+    'PageFooter': PageFooter
+  } // components 등록하기
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ myProject_1/page_01/src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template> <!--component는 무조건 하나의 태그로 감쌀 것-->
+  <div>
+      Header
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
