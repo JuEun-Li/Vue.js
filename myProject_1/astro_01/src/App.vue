6c2a51f33510788472af3cae184f8c9e44e5991a
JuEun-Li <wndms61@gmail.com>
2021-01-03 19:31:45 +0900

    setting

--- myProject_1/astro_01/src/App.vue
+++ myProject_1/astro_01/src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-  
+    <AstroHeader></AstroHeader>
+    <AstroBackground></AstroBackground>
+    <AstroMain></AstroMain>
+    <AstroFooter></AstroFooter>
    </div>
 </template>
 
 <script>
+ import AstroHeader from './components/AstroHeader.vue'
+ import AstroBackground from './components/AstroBackground.vue'
+ import AstroMain from './components/AstroMain.vue'
+ import AstroFooter from './components/AstroFooter.vue'
+  
 export default {
- 
+  components: {
+    'AstroHeader': AstroHeader,
+    'AstroBackground': AstroBackground,
+    'AstroMain': AstroMain,
+    'AstroFooter': AstroFooter
+  }
 }
 </script>
 
