fccae4aa926a0978dcbd8256bd51e936b10f8238
JuEun-Li <wndms61@gmail.com>
2021-01-04 01:27:41 +0900

    nav추가

--- myProject_1/astro_01/src/components/AstroHeader.vue
+++ myProject_1/astro_01/src/components/AstroHeader.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-      header
+      <nav>
+        <ul class="mainbar">
+          <li class="list"><a href="">home</a></li>
+          <li class="list"><a href="">개요</a></li>
+          <li class="list"><a href="">행성 이야기</a></li>
+          <li class="list"><a href="">소행성 & 위성</a></li>
+          <li class="list"><a href="">퀴즈</a></li>
+        </ul>
+      </nav>
   </div>
 </template>
 
@@ -10,6 +18,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.mainbar{
+  background: rgb(105, 137, 233);
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 15px;
+  margin: 0;
+  list-style-type: none;
+}
 
+.list {
+  padding-right: 50px;
+  cursor: pointer;
+}
+template {
+  margin: 0;
+}
 </style>
