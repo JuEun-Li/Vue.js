945b6459802dda08cf7c96ee18f9de43e9792eec
JuEun-Li <wndms61@gmail.com>
2021-01-18 00:47:20 +0900

    background 추가

--- myProject_1/astro_01/src/App.vue
+++ myProject_1/astro_01/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
+    <back-ground></back-ground>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import BackGround from './components/BackGround.vue';
 import ToolBar from './components/ToolBar.vue';
 
 export default {
   components: {
-    ToolBar
+    ToolBar,
+    BackGround
   }
 }
 </script>
---
+++ myProject_1/astro_01/src/components/BackGround.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+ div {
+   position: relative;
+    top: 0;
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    height: 250px;
+   background-color: aliceblue;
+   background-image:url('https://cdn.spacetelescope.org/archives/images/thumb700x/heic0709b.jpg');
+   /*Credit: NASA, ESA, Marc Postman (STScI), and the CLASH team. */
+ }
+
+ span {
+   position:absolute;
+  margin-top: 16%;
+  width: 100vw;
+  background: black;
+  height: 52px;
+  display: block;
+  /* flex-direction: row; */
+  /* list-style-type: none; */
+  opacity: 0.8; /*흐리게 조절*/
+ }
+</style>
